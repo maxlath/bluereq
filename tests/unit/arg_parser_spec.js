ac90268bd1633f1c603e90586eb4558954d21725
maxlath <k@maxlath.eu>
2019-11-03 23:36:00 +0100

    cleanup argument parsing

--- tests/unit/arg_parser_spec.js
+++ tests/unit/arg_parser_spec.js
@@ -1,39 +1,36 @@
 const should = require('should')
-const getOpts = require('../../lib/get_options')
+const getOptions = require('../../lib/get_options')
 
 describe('argParser', () => {
-  describe('getOpts', () => {
+  describe('getOptions', () => {
     describe('[url]', () => {
       it('should return a valid options object', () => {
         const args = ['http://example.dev']
-        should(getOpts(args).config).deepEqual({ url: 'http://example.dev', json: true, gzip: true })
-        should(getOpts(args).callback).not.be.ok()
+        should(getOptions(args)).deepEqual({ url: 'http://example.dev', json: true, gzip: true })
       })
     })
 
     describe('[config]', () => {
       it('should return a valid options object', () => {
         const args = [{ url: 'http://example.dev' }]
-        const opts = getOpts(args)
-        should(opts.config).deepEqual(args[0])
-        should(opts.callback).not.be.ok()
+        const options = getOptions(args)
+        should(options).deepEqual(args[0])
       })
     })
 
     describe('[url, data]', () => {
       const args = ['http://example.dev', { message: 'JSON data' }]
       it('should return a valid options object', () => {
-        const opts = getOpts(args, true)
-        should(opts.config).deepEqual({ url: args[0], json: args[1], gzip: true })
-        should(opts.callback).not.be.ok()
+        const options = getOptions(args, true)
+        should(options).deepEqual({ url: args[0], json: args[1], gzip: true })
       })
     })
 
     describe('[url, string data]', () => {
       const args = ['http://example.dev', 'string data']
       it('should return without json option', () => {
-        const opts = getOpts(args, true)
-        should(opts.config).deepEqual({ url: args[0], body: args[1], gzip: true })
+        const options = getOptions(args, true)
+        should(options).deepEqual({ url: args[0], body: args[1], gzip: true })
       })
     })
   })
